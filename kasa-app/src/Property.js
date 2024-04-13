64a8319028b896de2f8987b93d225f41b81ef77b
Jules Vérité <julesvrte2208@gmail.com>
2024-04-13 02:58:00 +0200

    fix css and function previousSlider

--- kasa-app/src/Property.js
+++ kasa-app/src/Property.js
@@ -14,26 +14,28 @@ const Property = () => {
     function previousSlide() {
         const container = document.querySelector('.container');
 
-        if(currentSlide > 0) {
+        if (currentSlide === 0) {
+            container.style.transform = `translate(${(pictureNumbers-1)*-93}vw)`
+            currentSlide = pictureNumbers - 1;
+        }
+        
+        else if(currentSlide > 0) {
             currentSlide--;
-           
+            container.style.transform = `translate(${currentSlide*-93}vw)`;
+            
         } else {
             currentSlide = pictureNumbers - 1;
-            container.style.transform = `translate(0vw)`;
-            return
+            container.style.transform = `translate(${currentSlide*93}vw)`
         }
 
-        container.style.transform = `translate(${currentSlide*93}vw)`;
     }
 
     function nextSlide() {
         const container = document.querySelector('.container');
 
-        if(currentSlide > -pictureNumbers + 1) {
+        if(currentSlide < pictureNumbers-1) {
             currentSlide++;
-
         } else {
-
             currentSlide = 0;
             container.style.transform = `translate(0vw)`
             return
