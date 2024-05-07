917d44c5efd1cd9bcd671ded781ca267ca06d9c8
Jules Vérité <julesvrte2208@gmail.com>
2024-05-07 10:58:17 +0200

    Revert "ajout de la gestion de l'apparition des fleches du slider"
    This reverts commit 82a10de7baacbdcf65a2a553625233f659a786e8.

--- kasa-app/src/components/PropertySlider.js
+++ kasa-app/src/components/PropertySlider.js
@@ -57,43 +57,31 @@ const PropertySlider = ({propertyPictures}) => {
                 ))}
             </div>
 
-            {() => { 
-                if (pictureNumbers > 1) {
-                    return ( 
-                        <span className='arrow left' onClick={previousSlide}>
-                            <svg width="96" height="120" viewBox="0 0 96 120" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g clipPath="url(#clip0_299517_96)">
-                            <path d="M70.04 15.7831L62.92 8.70312L23.36 48.3031L62.96 87.9031L70.04 80.8231L37.52 48.3031L70.04 15.7831Z" fill="white"/>
-                            </g>
-                            <defs>
-                            <clipPath id="clip0_299517_96">
-                            <rect width="96" height="119.64" fill="white" transform="translate(0 0.303223)"/>
-                            </clipPath>
-                            </defs>
-                            </svg>
-                        </span>
-                    )
-                }
-            }}
-            
-            {() => { 
-                if (pictureNumbers > 1) {
-                    return (
-                        <span className='arrow right'onClick={nextSlide} >
-                            <svg width="96" height="121" viewBox="0 0 96 121" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g clipPath="url(#clip0_299517_100)">
-                            <path d="M25.96 81.3458L33.04 88.4258L72.64 48.8258L33.04 9.22583L25.96 16.3058L58.48 48.8258L25.96 81.3458Z" fill="white"/>
-                            </g>
-                            <defs>
-                            <clipPath id="clip0_299517_100">
-                            <rect width="96" height="119.64" fill="white" transform="translate(0 0.825928)"/>
-                            </clipPath>
-                            </defs>
-                            </svg>
-                        </span>
-                    )
-                }
-            }}
+            <span className='arrow left' onClick={previousSlide}>
+                <svg width="96" height="120" viewBox="0 0 96 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clipPath="url(#clip0_299517_96)">
+                <path d="M70.04 15.7831L62.92 8.70312L23.36 48.3031L62.96 87.9031L70.04 80.8231L37.52 48.3031L70.04 15.7831Z" fill="white"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_299517_96">
+                <rect width="96" height="119.64" fill="white" transform="translate(0 0.303223)"/>
+                </clipPath>
+                </defs>
+                </svg>
+            </span>
+
+            <span className='arrow right'onClick={nextSlide} >
+                <svg width="96" height="121" viewBox="0 0 96 121" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clipPath="url(#clip0_299517_100)">
+                <path d="M25.96 81.3458L33.04 88.4258L72.64 48.8258L33.04 9.22583L25.96 16.3058L58.48 48.8258L25.96 81.3458Z" fill="white"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_299517_100">
+                <rect width="96" height="119.64" fill="white" transform="translate(0 0.825928)"/>
+                </clipPath>
+                </defs>
+                </svg>
+            </span>
 
         </div> );
 }
